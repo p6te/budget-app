f9601b046e23d56816a13bf8b20eee3f75daaa95
Piotr Matlak <piotr.matlak.contact@gmail.com>
2022-06-05 10:43:11 +0200

    add modals of ad budget and add enxpense

---
+++ src/components/AddBudgetModal.js
@@ -0,0 +1,56 @@
+import { Modal, Form, Button } from "react-bootstrap";
+import { useRef } from "react";
+import { useBudgets } from "../contexts/BudgetsContext";
+
+export default function AddBudgetModal({ show, handleClose }) {
+  const nameRef = useRef();
+  const maxRef = useRef();
+  const { addBudget } = useBudgets();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    addBudget({
+      name: nameRef.current.value,
+      max: parseFloat(maxRef.current.value),
+    });
+    handleClose();
+    
+  }
+
+  return (
+    <Modal show={show} onHide={handleClose}>
+      <Form onSubmit={handleSubmit}>
+        <Modal.Header closeButton>
+          <Modal.Title>New budget</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form.Group>
+            <Form.Label className="mb-3" controlId="name">
+              Name
+            </Form.Label>
+            <Form.Control ref={nameRef} type="text" required />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label className="mb-3" controlId="max">
+              Maximum spending
+            </Form.Label>
+            <Form.Control
+              ref={maxRef}
+              type="number"
+              required
+              min={0}
+              step={0.01}
+            />
+          </Form.Group>
+
+          <div className="d-flex justify-content-end mt-3">
+            <Button variant="primary" type="submit">
+              Add
+            </Button>
+          </div>
+        </Modal.Body>
+      </Form>
+    </Modal>
+  );
+}
---
+++ src/components/AddExpenseModal.js
@@ -0,0 +1,78 @@
+import { Modal, Form, Button } from "react-bootstrap";
+import { useRef } from "react";
+import {
+  useBudgets,
+  UNCATEGORIZED_BUDGET_ID,
+} from "../contexts/BudgetsContext";
+
+export default function AddExpenseModal({
+  show,
+  handleClose,
+  defaultBudgetId,
+}) {
+  const descriptionRef = useRef();
+  const amountRef = useRef();
+  const budgetIdRef = useRef();
+  const { addExpense, budgets } = useBudgets();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    addExpense({
+      descriptionRef: descriptionRef.current.value,
+      amount: parseFloat(amountRef.current.value),
+      budgetId: budgetIdRef.current.value,
+    });
+    handleClose();
+  }
+
+  return (
+    <Modal show={show} onHide={handleClose}>
+      <Form onSubmit={handleSubmit}>
+        <Modal.Header closeButton>
+          <Modal.Title>New expense</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form.Group>
+            <Form.Label className="mb-3" controlId="description">
+              Description
+            </Form.Label>
+            <Form.Control ref={descriptionRef} type="text" required />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label className="mb-3" controlId="amount">
+              Amount
+            </Form.Label>
+            <Form.Control
+              ref={amountRef}
+              type="number"
+              required
+              min={0}
+              step={0.01}
+            />
+          </Form.Group>
+
+          <Form.Group>
+            <Form.Label className="mb-3" controlId="budgetId">
+              Budget
+            </Form.Label>
+            <Form.Select defaultValue={defaultBudgetId} ref={budgetIdRef}>
+              <option id={UNCATEGORIZED_BUDGET_ID}>Uncategorized</option>
+              {budgets.map((budget) => (
+                <option key={budget.id} value={budget.id}>
+                  {budget.name}
+                </option>
+              ))}
+            </Form.Select>
+          </Form.Group>
+
+          <div className="d-flex justify-content-end mt-3">
+            <Button variant="primary" type="submit">
+              Add
+            </Button>
+          </div>
+        </Modal.Body>
+      </Form>
+    </Modal>
+  );
+}
